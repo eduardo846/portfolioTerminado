15760818d0a26ccf817a50acc5722c53097c5a1e
HectorRamirez <eduardo.846@gmail.com>
2023-01-17 07:16:30 -0400

    first commit

---
+++ src/components/NavBar/Navbar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import * as S from "./Navbar.styles";
+
+const Navbar = () => {
+  return <S.ContainerNavbar>Nuestro primer container Navbar</S.ContainerNavbar>;
+};
+
+export default Navbar;
