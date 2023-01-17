45abc0fc343f746823932a72bd2fa4501fc4023e
HectorRamirez <eduardo.846@gmail.com>
2023-01-17 08:35:28 -0400

    feat: add menu

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import * as S from "./Navbar.styles";
 
 const Navbar = () => {
-  return <S.ContainerNavbar>Nuestro primer container Navbar</S.ContainerNavbar>;
+  const [showMenuBurguer, setShowMenuBurguer] = useState(false);
+
+  return (
+    <S.ContainerNavbar>
+      <S.ContainerLogo>
+        <S.ImageLogo src="/img/logo.jpg" alt="Logo Hector Ramirez" />
+        <div>
+          <h1>Héctor Ramírez</h1>
+          <h2>Frontend Developer</h2>
+        </div>
+      </S.ContainerLogo>
+      <S.ContainerBurguer onClick={() => setShowMenuBurguer(!showMenuBurguer)}>
+        <S.BurguerMenu />
+        <S.BurguerMenu />
+        <S.BurguerMenu />
+      </S.ContainerBurguer>
+
+      <S.ContainerItems showMenuBurguer={showMenuBurguer}>
+        <p>About me</p>
+        <p>Projects</p>
+        <S.Button>Contact me</S.Button>
+      </S.ContainerItems>
+    </S.ContainerNavbar>
+  );
 };
 
 export default Navbar;
