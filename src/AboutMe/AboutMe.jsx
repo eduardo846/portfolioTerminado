6d0f8a72e461b6a3996778f70f721daa2281a869
HectorRamirez <eduardo.846@gmail.com>
2023-01-17 20:47:39 -0400

    feat: add pag sobre mi

---
+++ src/AboutMe/AboutMe.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import * as S from "./AboutMe.styles";
+
+const AboutMe = () => {
+  return (
+  <S.ContainerAboutMe>
+    <S.ContainerFlex>
+    <S.ContainerText>
+        <S.Title>
+            <p>Hello, i'm</p>
+            <p>Hector Ramirez</p>
+            <p>Full Stack</p>
+        </S.Title>
+    </S.ContainerText>
+    </S.ContainerFlex>
+    </S.ContainerAboutMe>);
+};
+
+export default AboutMe;
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import AboutMe from "./AboutMe/AboutMe";
+import Container from "./components/Container/Container";
 import Navbar from "./components/NavBar/Navbar";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Container>
+        <AboutMe />
+      </Container>
     </>
   );
 }
